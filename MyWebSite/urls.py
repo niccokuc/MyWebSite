0cfe79095f679d5ccf29c8f35c2a818c10d80f64
niccokuc <nerminkuc74@gmail.com>
2016-01-30 13:00:35 +0100

    Lecture 26

--- MyWebSite/urls.py
+++ MyWebSite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from posts.views import post_home
 
 urlpatterns = [
-    # url(r'^$', views.home, name='home')
     url(r'^admin/', admin.site.urls),
+    url(r'^posts/$', post_home),
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def posts_home(request):
+def post_home(request):
     return HttpResponse("<h1>Hello</h1>")
