158217621cfcdb5b8133a1ff408beee36fb90462
niccokuc <nerminkuc74@gmail.com>
2016-01-30 13:42:44 +0100

    Lecture 27

--- posts/views.py
+++ posts/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def post_home(request):
-    return HttpResponse("<h1>Hello</h1>")
+def post_create(request):
+    return HttpResponse("<h1>post_create</h1>")
+
+def post_detail(request):
+    return HttpResponse("<h1>post_detail</h1>")
+
+def post_list(request):
+    return HttpResponse("<h1>post_list</h1>")
+
+def post_update(request):
+    return HttpResponse("<h1>post_update</h1>")
+
+def post_delete(request):
+    return HttpResponse("<h1>post_delete</h1>")
