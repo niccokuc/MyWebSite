1e1d90e98f788721704eb20dc7a5d72eba242580
niccokuc <nerminkuc74@gmail.com>
2016-02-06 08:52:12 +0100

    Lecture 31

--- posts/views.py
+++ posts/views.py
@@ -1,25 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def post_create(request):
     return HttpResponse("<h1>post_create</h1>")
 
 def post_detail(request):
+    instance = get_object_or_404(Post, id=2)
     context_data = {
-            "title":"Detail"
+            "instance_list": instance,
+            "title":instance.title,
     }
-    return render(request, "index.html", context_data)
+    return render(request, "post_detail.html", context_data)
 
 def post_list(request):
+    queryset = Post.objects.all()
+    context = {
+        "object_list": queryset,
+        "title": "List"
+    }
     # if request.user.is_authenticated():
     #     context_data = {
     #         "title": "My User List"
     #     }
-    context_data = {
-        "title": "List"
-    }
-    return render(request, "index.html", context_data)
+    # context_data = {
+    #     "title": "List"
+    # }
+    return render(request, "index.html", context)
 
 def post_update(request):
     return HttpResponse("<h1>post_update</h1>")
